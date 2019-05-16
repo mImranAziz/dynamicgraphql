c16a01841924d68859e32133fa91aadc27e90661
Imran Aziz <imran.aziz@intelex.com>
2019-05-16 10:55:00 -0400

    [ILX-16975] Query the records of custom v6 object

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ const qs = require('querystring')
 
 const getObjectData = (entity, options) => {
   let url = `http://torwdiaziz01/devlogin/iadevform/api/v2/object/${entity}`
-  console.log(entity);
-  console.log(options);
   if (options) {
     url += '?' + qs.stringify(options)
   }
@@ -17,52 +15,17 @@ const getObjectData = (entity, options) => {
   })
 }
 
-let tabs = [
-  {
-    id: '2',
-    caption: 'My Tasks',
-    index: 1,
-  },
-  {
-    id: '3',
-    caption: "My Staff's Tasks",
-    index: 2,
-  },
-]
-
-let views = [
-  {
-    id: '4',
-    source: '...',
-  },
-]
-
-let myTasksModule = {
-  id: '1',
-  urlCaption: 'MyTasks',
-  caption: 'My Tasks',
-  tabs: tabs,
-  view: views[0],
-}
-
 const resolvers = {
   Query: {
-    Application: (a, { urlCaption }) => {
+    Object: (a, { name }) => {
       return getObjectData('Eee_graphqlObject')
         .then(res => res.json())
-        .then(json => json.value[0])
+        .then(json => json.value)
         .then(rec => {
-          return {
-            id: rec.Id,
-            caption: rec.rating,
-            urlCaption: rec.show,
-            tabs: [],
-            view: views[0],
-          }
+          return rec;
         })
     },
   },
-  Tab: () => undefined,
 }
 
 const server = new GraphQLServer({
